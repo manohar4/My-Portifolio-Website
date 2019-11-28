0837df1b6ab1bb68554ece2534bf377e3ca675f6
manohar4 <manoharmanu4444@gmail.com>
2019-11-28 18:20:52 +0530

    Download IconsSet Feature and Progress bar added

---
+++ Assests/js/script.js
@@ -0,0 +1,28 @@
+ var pg = 0; //progress bar width initially
+        document.onreadystatechange = function() {
+            if (document.readyState == "interactive") {
+                var allElem = $("*");
+                var length = allElem.length;
+                for (var i = 0; i < length; i++) {
+                    set_elem(allElem[i], length);
+                }
+            }
+        }
+
+        function set_elem(elem, total) {
+            var percent_inc = 100 / total; //equally divide 100 among all elements
+
+            //if element is loaded
+            if ($(elem).length == 1) {
+                $('#bar').animate({
+                    width: pg + percent_inc + "%"
+                }, 10, function() {
+                    if (document.getElementById("bar").style.width == "100%") {
+                        //hide the bar
+                        $('#bar').fadeOut(350);
+                    }
+                });
+                //update the previous width value 
+                pg = pg + percent_inc;
+            }
+        }
