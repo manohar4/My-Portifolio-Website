cb036ab7c006a201453722de1aa5e91decef2065
manohar4 <manoharmanu4444@gmail.com>
2019-12-02 18:20:44 +0530

    project 3 Started and Progress bar updated

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,17 +1,21 @@
  var pg = 0; //progress bar width initially
  document.onreadystatechange = function () {
      if (document.readyState == "interactive") {
+         
          var allElem = $("*");
          var length = allElem.length;
          for (var i = 0; i < length; i++) {
              set_elem(allElem[i], length);
          }
+         
      }
+     
  }
 
  function set_elem(elem, total) {
+     
      var percent_inc = 100 / total; //equally divide 100 among all elements
-
+     
      //if element is loaded
      if ($(elem).length == 1) {
          $('#bar').animate({
@@ -25,6 +29,7 @@
          //update the previous width value 
          pg = pg + percent_inc;
      }
+     
  }
 
 
